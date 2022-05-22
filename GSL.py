daeac2ed07c59cf233fdc0e29438a1d79c531ebd
Won Jun Kim <glistering96@gmail.com>
2022-05-22 15:11:42 +0900

    implementation done

--- GSL.py
+++ GSL.py
@@ -8,16 +8,18 @@ MAX_ITER = 10**4
 1. a0, a1, a2, a3의 초기값으로 시작한 뒤에 a0 > a1 and a1 < a2인 지점을 찾을 때까지 a4, a5 ... 이런 식으로 구간을 재설정함
 2. 위의 구간을 찾으면 구간 l과 u를 구간으로 설정 한 뒤에 해당 구간에서 최소값 탐색
 
+이 알고리즘은 Introduction to Optimum Design 4th, p.440의 "Algorithm for 1D Search by Golden Sections"를 구현한 코드.
+
+PPT 강의자료와 다르게 초기 bracket을 설정하기 때문에 일부 다른 차이가 있을 수 있음.
+
+e.g.) ppt: f(a0)=f(delta) 부터 구간 설정, 책: f(0)부터 시작하며 f(a0)=f(delta) ...  순으로 구간 설정.
+
+그러나 주어진 과제의 2번 문제의 제한사항을 지키며 풀기 위해서는 책의 구현체를 따라 설정해야지 풀림. 따라서 책의 방법을 구현함.
+
 """
 
 
-def GSL(f, delta=0.1, eps=10**-5):
-    """
-    Start with four candidate points: a0, a1, a2, u
-    Algorithms for unbounded search. Keep expand bracket dimension
-
-    """
-
+def run_golden_search(f, delta=0.1, eps=10 ** -5, verbose=False, MAX_ITER = 10000):
     KSI = (1+np.sqrt(5))/2  # golden ratio
     TAU = (np.sqrt(5)-1)/2
 
@@ -28,20 +30,21 @@ def GSL(f, delta=0.1, eps=10**-5):
 
         return _a0, _a1, _a2
 
-    a0 = delta
-    a1 = a0+delta*KSI
-    a2 = a1+delta*KSI**2
+    a0 = 0
+    a1 = delta
+    a2 = a1+delta*KSI
     q = 3
 
     # print(f"a0: {a0}, a1: {a1}, a2: {a2}")
-    while f(a0) > f(a1) and f(a1) > f(a2):
 
+    while f(a0) > f(a1) and f(a1) > f(a2):
         a0, a1, a2 = _bracket(a1, a2, q)
         q += 1
         # print(f"a0: {a0}, a1: {a1}, a2: {a2}")
 
-    print(f"a0: {a0}, a1: {a1}, a2: {a2}")
-    print("-"*100)
+    # if verbose:
+    #     print(f"a0: {a0}, a1: {a1}, a2: {a2}")
+    # print("-"*100)
 
     # reduce interval
     l, a, b, u = a0, 0, 0, a2
@@ -49,10 +52,11 @@ def GSL(f, delta=0.1, eps=10**-5):
     d = (u-l)
     b = l + TAU * d
     a = l + (1 - TAU) * d
-    MAX_ITER = 2
 
     i = 0
-    print(f"Initial: l: {l}, a: {a}, b: {b}, u: {u}")
+
+    # if verbose:
+    #     print(f"Initial: l: {l}, a: {a}, b: {b}, u: {u}")
 
     while abs(u-l) > eps and i < MAX_ITER: # stop if the interval becomes smaller than eps
         if f(a) > f(b):
@@ -73,17 +77,24 @@ def GSL(f, delta=0.1, eps=10**-5):
             a = l + (1-TAU)*d
 
         i += 1
-        print(f"No: {i} l: {l}, a: {a}, b: {b}, u: {u}")
+    #     if verbose:
+    #         print(f"Iter_{i}: l: {l}, a: {a}, b: {b}, u: {u}")
+    #
+    # if verbose:
+    #     print(f"l: {l}, a: {a}, b: {b}, u: {u}")
 
     return (u+l)/2
 
 
 if __name__ == '__main__':
     def opt_f(x):
-        return 24*x**2-24*x+6
+        return x**4-14*x**3+60*x**2-70*x
 
     def f(x):
         return x[0]**2+x[1]**2+x[2]**2
+
+    # def f(x):
+    #     return x**4-14*x**3+60*x**2-70*x
 
     x0 = np.array([1,2,-1])
     d = np.array([-2,-4,2])
@@ -97,8 +108,18 @@ if __name__ == '__main__':
             # cal descent direction
             pass
 
-        step_size = GSL(f, delta=0.05)
+        step_size = run_golden_search(f, delta=0.05, verbose=True)
         return x0+step_size*d
 
-    x0 = descent(opt_f_auto, x0, d)
-    print(f"updated x: {x0} with value: {f(x0)}")
+    run_golden_search(opt_f, delta=0.05, verbose=True, eps=0.001)
+
+    # xt = descent(opt_f_auto, x0, d)
+    # print(f"updated x: {xt} with value: {f(xt)}")
+
+    # xt = descent(opt_f, x0, d)
+    # print(f"updated x: {xt} with value: {f(xt)}")
+
+    # def f(x):
+    #     return x**4 - 14*x**3 + 60*x**2 -70*x
+    #
+    # print(GSL(f, verbose=True))
--- descent.py
+++ descent.py
@@ -1,80 +1,150 @@
 import numpy as np
-from GSL import GSL
+from GSL import run_golden_search
+
 
 class GradientDescent:
-    def __init__(self, f, g_f=None, type="steepest", eps=0.005):
+    def __init__(self, f, type="steepest", verbose=False, eps=0.005, delta=0.0001):
         """
         f: objective
-        g_f: gradient of f
         """
         self.f = f # objective function
         self.type = type
         self.func_call_count = 0
-        self.g_f = g_f
         self.eps = eps
+        self.verbose = verbose
+        self.delta = delta
+        self.num_iter = 0
 
-    def _diff(self, x: np.ndarray, h=10**-6):        # numerical partial differentiation
+    def _diff(self, x: np.ndarray, h=10**-8):        # numerical partial differentiation
         grad = np.zeros(x.shape[-1])
+        h_vec = np.zeros(x.shape[-1])
 
-        for i in grad.shape[-1]:
-            x_1 = np.copy(grad)
-            x_1[i] += h
-            grad[i] = (self.f(x_1)-self.f(x))/h # forward difference (f(x+h) - f(x))/h
+        for i in range(grad.shape[-1]):
+            h_vec[i] = h
+            grad[i] = (self.f(x+h_vec)-self.f(x))/h # forward difference (f(x+h) - f(x))/h
             self.func_call_count += 2
+            h_vec[i] = 0
 
+        # print(f"grad: {grad}")
         return grad
 
-    def descent(self, x, delta=0.05, line_search_obj=None):
+    def descent(self, x, delta=0.05):
         if self.type == "steepest":
-            x_opt = self._steepest_descent(x, delta, line_search_obj)
+            x_opt = self._steepest_descent(x, delta)
 
         else:
-            x_opt = self._conjugate_descent(x, delta, line_search_obj)
+            x_opt = self._conjugate_descent(x, delta)
 
         func_value = self.f(x_opt)
-        print(f"Opt x: {x_opt}, func_value: {func_value}")
+        print(f"Opt x: {x_opt}, func_value: {func_value}, num_iter: {self.num_iter}, "
+              f"num_func_calls: {self.func_call_count}")
 
         return x_opt, func_value
 
-    def _steepest_descent(self, x, delta, line_search_obj):
+    def _steepest_descent(self, x, delta):
         dir = -self._diff(x)
 
-        while np.linalg.norm(dir) < self.eps:
-            if line_search_obj:
-                step_size = GSL(line_search_obj, delta, )
-
-            else: step_size = 0.01
-
-            x = x + step_size*dir
+        num_iter = 0
+        while np.linalg.norm(dir) > self.eps:
             dir = -self._diff(x)
 
+            def opt_f_auto(p):
+                func_val = self.f(x + dir * p)
+                return func_val
+
+            step_size = run_golden_search(opt_f_auto, delta, verbose=self.verbose)
+
+            x = x + step_size*dir
+            func_value = self.f(x)
+            num_iter += 1
+
+            if self.verbose:
+                print(f"Opt x: {x}, func_value: {func_value}, step_size: {step_size}, norm: {np.linalg.norm(dir)}\n")
+
+        self.num_iter = num_iter
         return x
 
-    def _conjugate_descent(self, x, delta, line_search_obj):
-        # x: initial point
+    def _conjugate_descent(self, x, delta):
+        # x: initial point given
         c = self._diff(x)
-        beta = None
+        first_iter = True
+        dir = None
 
-        def opt_f_auto(p, dir):
-            return self.f(x + dir * p)
-
-        while np.linalg.norm(c) < self.eps:
-            if beta is None:
+        num_iter = 0
+        while np.linalg.norm(c) > self.eps:
+            if first_iter:
                 dir = -c
+                first_iter = False
 
             else:
-                c_next = self._diff(x)
-                beta = c_next.T.dot(c_next) / c.T.dot(c)
+                c_prev = np.copy(c)
+                c = self._diff(x)
+                beta = np.dot(c.T, c) / np.dot(c_prev.T, c_prev)
                 dir = -c + beta*dir
 
+            def opt_f_auto(p):
+                return self.f(x + dir * p)
 
-
-            if line_search_obj:
-                step_size = GSL(line_search_obj, delta)
-
-            else:
-                step_size = 0.01
+            step_size = run_golden_search(opt_f_auto, delta, verbose=self.verbose)
 
             x = x + step_size * dir
+            func_value = self.f(x)
+            num_iter += 1
 
+            if self.verbose:
+                print(f"Opt x: {x}, func_value: {func_value}, step_size: {step_size}, norm: {np.linalg.norm(c)}\n")
+
+        self.num_iter = num_iter
         return x
+
+
+if __name__ == '__main__':
+    # def f(x):
+    #     x1 = x[0]
+    #     x2 = x[1]
+    #
+    #     return 50*(x2-x1**2)**2+(2*x1)**2
+    #
+    # x0 = np.array([5, -5])
+
+    def f(x):
+        x1, x2, x3, x4 = x
+        result = (x1+10*x2)**2
+        result += 5*(x3-x4)**2
+        result += (x2-2*x3)**4
+        result += 10*(x1-x4)**4
+        return result
+
+    x0 = np.array([3, -1, 0, 1])
+
+    # def f(x):
+    #     x1 = x[0]
+    #     x2 = x[1]
+    #     x3 = x[2]
+    #     return x1**2+2*x2**2+2*x3**2+2*x1*x2+2*x2*x3
+    # x0 = np.array([2, 4, 10])
+
+    # def f(x):
+    #     x1 = x[0]
+    #     x2 = x[1]
+    #     return 100*((x2-x1**2)**2)+(1-x1)**2
+    # x0 = np.array([5, 2])
+
+    eps = 0.005
+
+    gd_s = GradientDescent(f, verbose=False, eps=eps)
+    opt_x = gd_s.descent(x0, delta=0.05)
+
+    gd_c = GradientDescent(f, verbose=False,type='conjugate', eps=eps)
+    opt_x = gd_c.descent(x0, delta=0.05)
+
+    # def f(x):
+    #     x1=x[0]
+    #     x2=x[1]
+    #
+    #     return 20*(x1-x2)**2
+    #
+    # x0 = np.array([2, 1])
+    #
+    # gd = GradientDescent(f)
+    # print(gd._diff(x0))
